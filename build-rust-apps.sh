2d2c8d4b9d67a72564cef81f9d9649e49abdc19b
Erik Horton <hortonew@gmail.com>
2024-11-23 16:01:45 -0700

    Remove apply from build script since argocd handles it

--- build-rust-apps.sh
+++ build-rust-apps.sh
@@ -7,7 +7,6 @@ docker tag hortonew/rabbitmq-rust-app hortonew/rabbitmq-rust-app:latest
 docker tag hortonew/rabbitmq-rust-app hortonew/rabbitmq-rust-app:0.0.6
 docker push hortonew/rabbitmq-rust-app:latest
 docker push hortonew/rabbitmq-rust-app:0.0.6
-kubectl apply -f apps/2-rabbitmq-rust-app-producer/k8s/2-cronjob.yml
 
 # Consumer
 docker build -t hortonew/rabbitmq-rust-app-consumer apps/2-rabbitmq-rust-app-consumer
@@ -15,4 +14,3 @@ docker tag hortonew/rabbitmq-rust-app-consumer hortonew/rabbitmq-rust-app-consum
 docker tag hortonew/rabbitmq-rust-app-consumer hortonew/rabbitmq-rust-app-consumer:0.0.2
 docker push hortonew/rabbitmq-rust-app-consumer:latest
 docker push hortonew/rabbitmq-rust-app-consumer:0.0.2
-kubectl apply -f apps/2-rabbitmq-rust-app-consumer/k8s/deployment.yml
